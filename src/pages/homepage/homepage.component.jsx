e6fc6ee49815d8acadf3bf1f0d21ae6574fe1944
lei-diva <divaaleii@gmail.com>
2020-03-10 17:17:26 -0700

    routing with react dom

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,17 +1,10 @@
 import React from 'react';
 import './homepage.styles.scss';
-import { MenuItem } from '../../components/menu-item/menu-item.component';
 import Directory from '../../components/directory/directory.component';
 
 export const HomePage =() => (
     <div className="homepage">
         <Directory>
-            <MenuItem title="HATS"/>
-            <MenuItem title="JACKETS"/>
-            <MenuItem title="SNEAKERS"/>
-            <MenuItem title="WOMENS"/>
-            <MenuItem title="MENS"/>
-
         </Directory>
 
     </div>
